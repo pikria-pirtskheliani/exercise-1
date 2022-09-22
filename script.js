d3869f9179078936b7cd3883a2732765933f2f94
pikria-pirtskheliani <ffircxeliani@gmail.com>
2022-09-22 16:17:46 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,124 @@
+// // ---1--- //
+
+// let user = {
+//     firstname: "giorgi",
+//     lastname: "smith",
+//     age:25,
+//     studentstatus:"active"
+// };
+// console.log(user.studentstatus);
+
+
+
+
+// // ---2--- //
+
+// array = ["string", 15, "char", 2, 4, 6, "bool"]; 
+// // for (let item of array) {
+// //     console.log(item)
+// // } 
+// let z=0; 
+// while (z < array.length) {
+//     console.log(array[z])
+//     z++
+//     };
+ 
+
+
+
+// // ---3--- // 
+
+// let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+
+// for (let item of numbers) {
+//     if (item > 5) {
+//         console.log(item)
+//     }
+// };
+
+
+
+
+// //---4--- //
+
+// let user = {
+//     name: "giorgi",
+//     age: 20,
+//     studentstatus:"active"
+// }; 
+
+// if (user.age < 18 && user.studentstatus=="active") {
+//     console.log("hello");
+// } else if (user.name=="levani") {
+//     console.log("hello levani");
+// } else if (user.studentstatus=="active"|| user.age < 25) {
+//     console.log("hello world")
+// } else {
+//     console.log("error")
+// };
+
+
+
+// // ---5--- //
+
+// let array = ["watermelon", "pear", 10, 45 ,50, "apple", "ananas"]; 
+
+// for (let item of array) {
+//     if(typeof item=="string") {
+//          console.log(item)
+//     }
+// };
+
+
+
+// //---6--- // 
+
+// let array = [ [2, -3, 5, 10], [25, -24, -11, 100] , [-6, -7, 10] ];
+
+// for (let x of array[0] )
+// if (x > 0) {
+//     console.log(x)
+// };
+// for (let y of array[1] )
+// if (y > 0) {
+//     console.log(y)
+// };
+// for (let z of array[2] )
+// if (z > 0) {
+//     console.log(z)
+// };
+
+
+// // ---7--- //
+
+// let array = [ 2, 3, 5, 10, 25, 24, 11, 100 , 6, 7, 10 ]; 
+
+// for(let item of array) {
+//     if ( item % 2 == 1 ) {
+//         console.log(item);
+//     }
+// };                           //კენტი
+
+// for(let item of array) {
+//     if ( item % 2 == 0 ) {
+//         console.log(item);
+//     }
+// };                           //ლუწი
+
+
+
+// ---8--- //
+
+// let users = [
+//     {username: "giorgi", status: false},
+//     {username: "levani", status: false},
+//     {username: "anna", status: true}
+// ];
+
+// for(let x = 0; x < users.length; x++){
+//     if(users[x].status == true){
+//         console.log(users[x]);
+//     }
+// };
+
+
